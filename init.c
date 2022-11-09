906d7d0057b53322c499653120b360d4ac3138bb
PYticPH <pyticph@gmail.com>
2022-11-09 08:25:53 -0800

    Added: simple shell files

---
+++ init.c
@@ -0,0 +1,15 @@
+#include "shell.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: initialize shell ...
+ *
+ * Return: int
+ */
+
+int main(void)
+{
+	prompt();
+	return (0);
+}
---
+++ shell.h
@@ -0,0 +1,16 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+/**
+ * Fileetype - Header  file
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+
+void prompt(void);
+
+#endif
